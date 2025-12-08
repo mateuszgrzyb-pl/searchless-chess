333cfca26b0e8250be581da56e09fc09c58c350d
Mateusz Grzyb <mwgrzyb@gmail.com>
2025-12-08 22:30:21 +0100

    feat: new callbacks for TF models

---
+++ src/training/callbacks.py
@@ -0,0 +1,70 @@
+"""Custom Keras callbacks for training monitoring and control."""
+import keras
+
+from src.utils.tools import load_config
+
+
+def load_callbacks_from_config(logs_dir: str, checkpoint_dir: str, steps_per_epoch: int = None) -> list:
+    """
+    Factory function to create callbacks from YAML config.
+    
+    Args:
+        config_path: Path to model config YAML
+        checkpoint_dir: Directory for saving checkpoints
+        
+    Returns:
+        List of Keras callbacks
+    """
+    config = load_config('configs/config.yaml')['callbacks']
+    callbacks = []
+
+    # Backup every n-epochs
+    if config['epoch_checkpoint']['enabled'] is True:
+        save_every_epochs = config['epoch_checkpoint']['save_every']
+        if steps_per_epoch:
+            save_freq = int(save_every_epochs * steps_per_epoch)
+        else:
+            save_freq = 'epoch'
+
+        callbacks.append(
+            keras.callbacks.ModelCheckpoint(
+                filepath=f'{checkpoint_dir}/epoch_{{epoch:02d}}.keras',
+                save_freq=save_freq,
+                verbose=config['epoch_checkpoint']['verbose']
+            )
+        )
+
+    # Best model
+    if config['best_checkpoint']['enabled'] is True:
+        callbacks.append(
+            keras.callbacks.ModelCheckpoint(
+                filepath=f'{checkpoint_dir}/{config["best_checkpoint"]["filename"]}',
+                monitor=config['best_checkpoint']['monitor'],
+                save_best_only=True,
+                mode=config['best_checkpoint']['mode'],
+                verbose=config['best_checkpoint']['verbose']
+            )
+        )
+
+    # Early Stopping
+    if config['early_stopping']['enabled'] is True:
+        callbacks.append(
+            keras.callbacks.EarlyStopping(
+                monitor=config['early_stopping']['monitor'],
+                patience=config['early_stopping']['patience'],
+                restore_best_weights=config['early_stopping']['restore_best_weights'],
+                mode=config['early_stopping']['mode'],
+                min_delta=config['early_stopping']['min_delta'],
+                verbose=config['early_stopping']['verbose']
+            )
+        )
+
+    # TensorBoard
+    if config['tensorboard']['enabled']:
+        callbacks.append(
+            keras.callbacks.TensorBoard(
+                log_dir=logs_dir
+            )
+        )
+
+    return callbacks
